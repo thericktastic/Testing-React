b79409b97b2d0e55de70469f27065890b4ec0180
thericktastic <love@The-RT-MacBook-Pro.local>
2020-01-27 20:51:15 -0500

    Completed 4 tests - 2x button render, 2x new information upon button press

--- star-wars-app/src/components/StarWarsCharacters.test.js
+++ star-wars-app/src/components/StarWarsCharacters.test.js
@@ -1,38 +1,66 @@
 import React from "react";
 import { render, fireEvent, wait } from "@testing-library/react";
-import { getData } from "../api"; // getData is the component making the API call
+import { getData as mockGetData } from "../api"; // getData is the component making the API call
 import StarWarsCharacters from "./StarWarsCharacters";
 
 // jest.mock()
 // Anything that gets exported from "../api" is going to have a mock function created for it
-// jest.mock("../api");
+jest.mock("../api");
 
 const initialData = {
-    next: "https://swapi.co/api/people/?page=2",
-    previous: null
+  next: "https://swapi.co/api/people/?page=2",
+  previous: "null",
+  results: [
+    { name: "Luke Skywalker", url: "lukeURL" },
+    { name: "C-3P0", url: "cURL" }
+  ]
 };
 
 const mockData = {
-    next: "https://swapi.co/api/people/?page=2",
-    previous: "https://swapi.co/api/people/?page=1"
-}
+  next: "https://swapi.co/api/people/?page=2",
+  previous: "https://swapi.co/api/people/?page=1",
+  results: [
+    { name: "Anakin Skywalker", url: "darthURL" },
+    { name: "Wilhuff Tarkin", url: "wURL" }
+  ]
+};
 
-test("renders the StarWarsCharacters component with the prev button", () => {
+test("renders the StarWarsCharacters component with the prev button", async () => {
   // AAA Arrange, Act, Assert
+  mockGetData.mockResolvedValue(initialData);
   const { getByText } = render(<StarWarsCharacters />);
-  getByText("Previous");
+  await wait(() => expect(getByText(/previous/i)));
 });
 
-test("renders the StarWarsCharacters component with the next button", () => {
+test("renders the StarWarsCharacters component with the next button", async () => {
   // AAA Arrange, Act, Assert
+  mockGetData.mockResolvedValue(initialData);
   const { getByText } = render(<StarWarsCharacters />);
-  getByText("Next");
+  await wait(() => expect(getByText(/next/i)));
 });
 
-test("fires the next button to make another axios call", () => {
-  getData.mockResolvedValueOnce(initialData)
+test("previous button renders new data", async () => {
+  // AAA Arrange, Act, Assert
+  mockGetData.mockResolvedValue(mockData);
+
   const { getByText } = render(<StarWarsCharacters />);
-  const nextButton = getByText("Next");
-  fireEvent.click(nextButton);
-  expect(getData).toHaveBeenCalledTimes(1);
+  const prevButton = getByText(/Previous/i);
+
+  mockGetData.mockResolvedValue(initialData);
+
+  await wait(() => fireEvent.click(prevButton));
+  await wait(() => expect(getByText("Luke Skywalker")));
+});
+
+test("next button renders new data", async () => {
+  // AAA Arrange, Act, Assert
+  mockGetData.mockResolvedValue(initialData);
+
+  const { getByText } = render(<StarWarsCharacters />);
+  const nextButton = getByText(/Next/i);
+
+  mockGetData.mockResolvedValue(mockData);
+
+  await wait(() => fireEvent.click(nextButton));
+  await wait(() => expect(getByText("Anakin Skywalker")));
 });
